fbb4da7964f9b4f6704d42fab450968411ff3b6e
karl2015y <karl2015y@gmail.com>
2022-04-07 00:20:54 +0800

    使用解構拿個人資料

--- src/tools/liff.js
+++ src/tools/liff.js
@@ -6,11 +6,20 @@ liff.init({
     liffId: '1657035409-k7dgR7BZ', // Use own liffId
 }).then(() => {
     const userStatus = userStore();
-    userStatus.isLogin=liff.isLoggedIn()
-    if(!userStatus.isLogin){
+    userStatus.isLogin = liff.isLoggedIn()
+    if (!userStatus.isLogin) {
         liff.login()
-    }else{
+    } else {
         alert("登入成功")
-        userStatus.user=liff.getDecodedIDToken();
+        const { name, picture, sub, email } = liff.getDecodedIDToken();
+        userStatus.user = {
+            name: name,
+            picture: picture,
+            user_id: sub,
+            email: email
+        }
+
+
+
     }
 });
