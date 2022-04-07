04c79f74902b94a628737c2f3861171d7a861546
karl2015y <karl2015y@gmail.com>
2022-04-07 19:10:25 +0800

    抽獎雛形補上資料

--- src/view/game.vue
+++ src/view/game.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="w-screen h-screen flex justify-center items-center flex-col">
+  <div class="w-screen flex justify-center items-center flex-col">
     <h2>抽籤大賞</h2>
     <h1>{{ gameData.name }}</h1>
     <div v-if="gameData.gift.length > 0">
       <div v-if="current_gift">
         <h2>[目前抽出]</h2>
         <h1>{{ current_gift.name }}</h1>
+        <img :src="current_gift.img" class="w-56 h-auto" alt="" />
         <h3>剩餘數量：{{ current_gift.count }}</h3>
       </div>
+      <br />
       <div>
         <h2>[中獎人]</h2>
         <h1>{{ get_gift_person.graduation }}</h1>
@@ -33,15 +35,82 @@
     </div>
     <div v-else>抽獎結束</div>
   </div>
+
+    <h1>已中獎</h1>
+  <div class="flex gap-3 justify-around">
+    <div v-for="item in gameData.result" :key="item">
+      <div>
+        <h2>[抽種禮物]</h2>
+        <h1>{{ item.gift.name }}</h1>
+        <img :src="item.gift.img" class="w-56 h-auto" alt="" />
+      </div>
+      <div>
+        <h2>[中獎人]</h2>
+        <h1>
+          {{ item.person.graduation }}
+        </h1>
+        <h1>{{ item.person.name }}</h1>
+        <img
+          :src="item.person.picture"
+          class="
+            w-56
+            h-56
+            rounded-full
+            shadow
+            border-solid border-4
+            object-cover
+          "
+          :class="{
+            'border-red-500': item.person.gender == '女',
+            'border-blue-500': item.person.gender == '男',
+          }"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+
+  <van-popup v-model:show="show_get_give" closeable>
+    <div>
+      <h2>[抽種禮物]</h2>
+      <h1>{{ gameData.result[gameData.result.length - 1].gift.name }}</h1>
+      <img
+        :src="gameData.result[gameData.result.length - 1].gift.img"
+        class="w-56 h-auto"
+        alt=""
+      />
+    </div>
+    <div>
+      <h2>[中獎人]</h2>
+      <h1>
+        {{ gameData.result[gameData.result.length - 1].person.graduation }}
+      </h1>
+      <h1>{{ gameData.result[gameData.result.length - 1].person.name }}</h1>
+      <img
+        :src="gameData.result[gameData.result.length - 1].person.picture"
+        class="w-56 h-56 rounded-full shadow border-solid border-4 object-cover"
+        :class="{
+          'border-red-500':
+            gameData.result[gameData.result.length - 1].person.gender == '女',
+          'border-blue-500':
+            gameData.result[gameData.result.length - 1].person.gender == '男',
+        }"
+        alt=""
+      />
+    </div>
+  </van-popup>
 </template>
 
 
 <script>
 import { getGameData } from "@/api/game";
 import { computed, ref } from "vue";
+import { Popup } from "vant";
+
 export default {
+  components: { [Popup.name]: Popup },
   setup() {
-    const gameData = ref({ gift: [], members: [] });
+    const gameData = ref({ gift: [], members: [], result: [] });
     getGameData("aczWi3jxDcTT4V7hId67").then((data) => {
       gameData.value = data;
     });
@@ -62,6 +131,7 @@ export default {
     /**
      * 中獎人
      */
+    const show_get_give = ref(false);
     const playing = ref(false);
     const get_gift_person = ref({
       name: "",
@@ -79,7 +149,11 @@ export default {
 
         i++;
         if (i > times) {
-          console.log(get_gift_person.value, current_gift.value);
+          gameData.value.result.push({
+            person: get_gift_person.value,
+            gift: current_gift.value,
+          });
+          show_get_give.value = true;
           array.splice(randomIndex, 1); // 刪除已經中獎的
           current_gift.value.count--; // 刪除商品數量
           if (current_gift.value.count <= 0) {
@@ -104,6 +178,7 @@ export default {
       get_gift_person,
       randomGiveGift,
       playing,
+      show_get_give,
     };
   },
 };
