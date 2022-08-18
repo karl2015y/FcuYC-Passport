fbd50f49f82e15157b43a9408b91665f3ce570d4
karl2015y <karl2015y@gmail.com>
2022-08-18 18:34:34 +0800

    feat: 新增刷新按鈕

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,5 @@
 <template>
-  <div
-    class="
+  <div class="
       w-screen
       h-screen
       z-[9999]
@@ -11,28 +10,37 @@
       justify-center
       items-center
       bg-white bg-opacity-95
-    "
-  >
-    <lottie-player
-      class="-mt-40 w-96 h-96"
-      autoplay
-      loop
-      mode="normal"
-      src="/loading.json"
-    ></lottie-player>
+    ">
+    <lottie-player class="-mt-40 w-96 h-96" autoplay loop mode="normal" src="/loading.json"></lottie-player>
     <div class="flex font-bold text-2xl">
       <span id="loading-text" />
+    </div>
+    <div>
+      <button v-show="showReloadBtn" @click="handleReload()" class="
+          mt-3
+          px-5
+          bg-center bg-no-repeat bg-clip-padding bg-origin-padding
+          shadow-[0_0.769vw_1.538vw_#00000029]
+          rounded-[5.641vw]
+          text-[5vw]
+          font-bold
+          w-full
+          h-[10vw]
+        ">
+        重新整理
+      </button>
     </div>
   </div>
 </template>
 <script>
 import * as LottiePlayer from "@lottiefiles/lottie-player";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import Typed from "typed.js";
 
 export default {
   setup() {
     onMounted(() => {
+
       var typed = new Typed("#loading-text", {
         strings: [
           "載入中...",
@@ -41,10 +49,23 @@ export default {
           "好了好了真的好了...",
           "網路有點慢...",
         ],
-        typeSpeed:100,
+        typeSpeed: 100,
         loop: true,
       });
     });
+
+    const showReloadBtn = ref(false);
+      setTimeout(() => {
+        showReloadBtn.value = true
+      }, 5000);
+    const handleReload = () => {
+      localStorage.clear();
+      location.reload()
+    }
+    return {
+      handleReload,
+      showReloadBtn
+    }
   },
 };
 </script>
